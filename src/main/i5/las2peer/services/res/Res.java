2e0c4afa0e7ae565a66831a4e611f5da78327361
PhilCAEUser <mail@rwth>
2022-05-24 14:38:01 +0000

    Test

--- src/main/i5/las2peer/services/res/Res.java
+++ src/main/i5/las2peer/services/res/Res.java
@@ -71,9 +71,9 @@ public class Res extends RESTService {
 
   @Api
   @SwaggerDefinition(
-      info = @Info(title = "8", version = "$Metadata_Version$",
-          description = "$Metadata_Description$",
-          termsOfService = "$Metadata_Terms$",
+      info = @Info(title = "8", version = "",
+          description = "",
+          termsOfService = "",
           contact = @Contact(name = "", email = "CAEAddress@gmail.com") ,
           license = @License(name = "BSD",
               url = "https://github.com/PhilCAEOrg2/microservice-8/blob/master/LICENSE.txt") ) )
