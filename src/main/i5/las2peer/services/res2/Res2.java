f29dcd36879ba1bbe1acf2092280696b30c64203
PhilCAEUser <mail@rwth>
2022-05-25 09:59:34 +0000

    Disable auth

--- src/main/i5/las2peer/services/res2/Res2.java
+++ src/main/i5/las2peer/services/res2/Res2.java
@@ -71,7 +71,7 @@ public class Res2 extends RESTService {
 
   @Api
   @SwaggerDefinition(
-      info = @Info(title = "8", version = "",
+      info = @Info(title = "8", version = "0.0.0",
           description = "",
           termsOfService = "",
           contact = @Contact(name = "", email = "CAEAddress@gmail.com") ,
